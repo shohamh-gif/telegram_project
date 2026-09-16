a0d617556307c16d4b9c3245eb34c6890a2ac2eb
Owner <shohamh@edu.aac.ac.il>
2026-09-16 16:18:24 +0300

    Updated gitignore and secured token

---
+++ src/main/java/org/example/SurveyData.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.List;
+
+public class SurveyData {
+    private String question;
+    private List<String> answers;
+
+    public String getQuestion() {
+        return this.question;
+    }
+
+    public List<String> getAnswers() {
+        return this.answers;
+    }
+}
